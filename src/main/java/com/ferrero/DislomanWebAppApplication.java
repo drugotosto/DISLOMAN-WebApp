2f7bdb168afaeaf9352247428e81411a6d8173a0
Maurizio Ferrero <ferrero.maurizio84@gmail.com>
2017-11-11 21:39:12 +0100

    Initial commit - Inizio Sviluppo della WebApplication che farà da front-end per il progetto DISLOMAN

---
+++ src/main/java/com/ferrero/DislomanWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.ferrero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DislomanWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DislomanWebAppApplication.class, args);
+	}
+}
